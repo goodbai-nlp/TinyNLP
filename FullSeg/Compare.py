b4e7e8c32568d85700ba2cade58d32b6ab865209
muyeby <bxf_hit@163.com>
2016-05-25 22:53:25 +0800

    分词预处理

---
+++ FullSeg/Compare.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: ??
+@author: muyeby
+@contact: bxf_hit@163.com
+@site: http://muyeby.github.io
+@software: PyCharm
+@file: Compare.py
+@time: 16-5-25 下午10:50
+"""
