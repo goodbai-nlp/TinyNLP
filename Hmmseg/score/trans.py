aee4b8dc67f7c027027b0cf3a4b413b7fb929037
muyeby <bxf_hit@163.com>
2016-07-18 20:17:01 +0800

    修改结构

---
+++ Hmmseg/score/trans.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: ??
+@author: muyeby
+@contact: bxf_hit@163.com
+@site: http://muyeby.github.io
+@software: PyCharm
+@file: trans.py
+@time: 16-7-10 上午11:43
+"""
+
