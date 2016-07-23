85b3a915bf646be040e04bfe3404c94c4b2f061f
muyeby <bxf_hit@163.com>
2016-07-23 22:43:16 +0800

    Name_NER start

---
+++ Hmmseg/train/Name_CN.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: muyeby
+@contact: bxf_hit@163.com
+@site: http://muyeby.github.io
+@software: PyCharm
+@file: Name_CN.py
+@time: 16-7-23 下午10:19
+"""
+
+
+
+if __name__ == '__main__':
+    pass
