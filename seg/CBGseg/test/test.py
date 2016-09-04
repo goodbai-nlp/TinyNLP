80f7beb8a120cbbfa795bc6180c7e4930aff6a8a
muyeby <bxf_hit@163.com>
2016-09-04 21:00:56 +0800

    add test examples

---
+++ seg/CBGseg/test/test.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: muyeby
+@contact: bxf_hit@163.com
+@site: http://muyeby.github.io
+@software: PyCharm
+@file: test.py
+@time: 16-9-4 下午8:02
+"""
+import codecs
+import sys
+import time
+import os
+
+sys.path.append(os.getcwd()+'/../../../')
+sys.path.append(os.getcwd()+"/../")
+from model.NumNer import NumRec
+from model.CBGM import CBGM
+from model.PlaceNer import PlaceRec
+from model.NameNer import CNNAME,decode
+from config.config import Config
+
+TRAIN_FILE = '../train/data/pku_data.txt'
+USER_DICT = '../train/data/userdict.txt'
+TEST_FILE = 'test.txt'
+class Seg(object):
+    def __init__(self):
+        self.segger = CBGM()
+        self.Pner = PlaceRec()
+        self.Numner = NumRec()
+        self.cname = CNNAME()
+        self.cname.fit()
+        self.idict = {}
+        self.load_dict()
+    
+    def load_dict(self):
+        f = codecs.open(USER_DICT, 'r')
+        for line in f:
+            if sys.version < '3.0':
+                if not (type(line) is unicode):
+                    try:
+                        line = line.decode('utf-8')
+                    except:
+                        line = line.decode('gbk', 'ignore')
+            word = line.strip()
+            self.idict[word] = 1
+    
+    def train(self, fname):
+        fr = codecs.open(fname, 'r', 'utf-8')
+        data = []
+        for i in fr:
+            line = i.strip()
+            if not line:
+                continue
+            tmp = map(lambda x: x.split('/'), line.split())
+            data.append(tmp)
+        fr.close()
+        self.segger.train(data)
+    
+    def seg(self, sentence):
+        '''标签到汉字序列转换'''
+        ret = self.segger.tag(sentence)
+        res = []
+        tmp = ''
+        for i in ret:
+            if i[1] == 'e':
+                res.append(tmp + i[0])
+                tmp = ''
+            elif i[1] == 'b' or i[1] == 's':
+                if tmp:
+                    res.append(tmp)
+                tmp = i[0]
+            else:
+                tmp += i[0]
+        if tmp:
+            res.append(tmp)
+        return res
+
+
+def Name_Replace(namelist, sen):
+    for name in namelist.strip().split(' '):
+        index = 0
+        if len(name) >= 3:
+            while index < len(sen):
+                if index < len(sen) - 2 and sen[index] == name[0] and sen[index + 1] == name[1] and sen[index + 2] == \
+                        name[2]:
+                    tmp = name[1] + name[2]
+                    sen = sen[:index + 1] + [tmp] + sen[index + 3:]
+                    index += 1
+                index += 1
+    return sen
+
+
+if __name__ == '__main__':
+    print(time.strftime('%Y-%m-%d %H:%M:%S'))
+    print('build model...')
+    seg = Seg()
+    seg.train(TRAIN_FILE)
+    test_sentence = open(TEST_FILE,'rb')
+    for line in test_sentence:
+        if sys.version < '3.0':
+            if not (type(line) is unicode):
+                try:
+                    line = line.decode('utf-8')
+                except:
+                    line = line.decode('gbk', 'ignore')
+        line = line.strip()
+        print '原：',line
+        res = seg.seg(line)
+        res1 = seg.Numner.NumNer(res)
+        res2 = seg.Pner.Place_Ner(res1)
+        namelist = decode(seg.cname,res2)
+        res3 = Name_Replace(namelist,res2)
+        ans = '/ '.join(res1)
+        print ans
+    
---
+++ seg/CRF++-0.58/seg/readme.txt
@@ -0,0 +1,3 @@
+========================CRF++分词器====================
+使用教程：
+1.跑语料： python crf_seg.py crf_model msr_test.utf8 output.txt
